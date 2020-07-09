5fd20e1fd53466a3cafb5669770ffcdb0e5c8841
Richard Joffray <rjoffray@richards-mbp.lan>
2020-07-09 09:58:02 -0700

    update to oak node module 6.0.0 bumping version to 1.0.8

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 const oak = require('oak')
 const waitOn = require('wait-on');
 let dns = require('dns')
+let window = null
 
 require('dotenv').config()
+
+console.log("Versions: ",JSON.stringify(process.versions, null, 2))
 
 function loadWindow () {
   
       let exceptions = ['localhost']
-      if (process.env.SSL_EXCEPTIONS) {
-        exceptions = process.env.SSL_EXCEPTIONS.split(';')
-      }
-      console.log("sslExceptions: ", exceptions)
-      oak.load({
-        url: process.env.REMOTE_URL || 'https://zivelo.com/',
-        size: process.env.WINDOW_SIZE || '1080x1920',
-        background: process.env.BACKGROUND_COLOR || '#ffffff',
-        ontop: !!+process.env.WINDOW_ONTOP,
+
+      let opts = {
+        url: process.env.URL || 'https://zivelo.com/',
+        background: process.env.COLOR || '#ffffff',
+        display: parseInt(process.env.DISPLAY) || 0 ,
+        ontop: Boolean(process.env.ONTOP) || false,
+        size: process.env.SIZE || "1920x1080",
         sslExceptions:  exceptions,
-        insecure: !!+process.env.INSECURE
-      })
+        insecure: Boolean(process.env.INSECURE)  || false,
+        kiosk: Boolean(process.env.KIOSK) || false,
+        fullscreen: Boolean(process.env.FULLSCREEN) || false
+      }
+      if (opts.fullscreen) {
+        delete opts.size
+      }
+
+      // let opts = process.env
+      if (process.env.SSL_EXCEPTIONS) {
+        opts.sslExceptions = process.env.SSL_EXCEPTIONS.split(';')
+      }
+
+      console.log("Options: ", opts)
+
+      window = oak.load(opts)
 
 }
 
 // everything has to wait for the main ready event to fire
 oak.on('ready', () => {
-  let waitFor = [process.env.REMOTE_URL];
+  let waitFor = [process.env.URL];
   if (process.env.WAIT_ON) {
     waitFor = process.env.WAIT_ON.split(";")
   }
